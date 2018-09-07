857d24bc4186cf635dbac0be23900a149bbfc5f4
Sessay <zxying_no1@163.com>
2018-09-07 11:49:18 +0800

    mint-ui

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+      <mt-button type="default" @click="test">default</mt-button>
+      <mt-button type="primary">primary</mt-button>
+      <mt-button type="danger">danger</mt-button>
     </div>
     <router-view/>
   </div>
 </template>
+<script>
+import { Toast } from 'mint-ui'
+export default {
+  methods: {
+    test () {
+      Toast({
+        message: '提示',
+        position: 'bottom',
+        duration: 5000
+      })
+    }
+  }
+}
+</script>
 
 <style lang="less">
 #app {
